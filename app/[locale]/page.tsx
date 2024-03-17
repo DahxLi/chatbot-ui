b481119e16abfca5ae8a8b7498cb980eb4982978
a not special CSc 110 student <59388781+DahxLi@users.noreply.github.com>
2024-03-16 22:02:19 -0400

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
         <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
       </div>
 
-      <div className="mt-2 text-4xl font-bold">Digtial Pluse 开发 GPT-4.0 聊天机器人｜Aura airtech 测试版本最新</div>
+      <div className="mt-2 text-4xl font-bold">Digtial Pluse 开发 GPT-4.0 聊天机器人 Aura airtech 测试版本最新</div>
 
       <Link
         className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
